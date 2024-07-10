377cd5263655dea79ee8e3015eb9e4abc9d72c61
briverse17 <55708719+briverse17@users.noreply.github.com>
2024-07-10 01:34:29 +0000

    add problem 1598

---
+++ 1598/README.md
@@ -0,0 +1,14 @@
+# 1598. Crawler Log Folder
+
+## Description
+
+Link: https://leetcode.com/problems/crawler-log-folder/
+
+## Solution
+See `sol.py`. If you need detail explanation please create an issue.
+
+## Complexity
+- Time: $$O(N)$$
+
+---
+Personal note: Daily. Jul 10 2024
---
+++ 1598/sol.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minOperations(self, logs: List[str]) -> int:
+        # monitor the height (or depth, whichever)
+        # of the directory
+        # `height = 0` means we are at "Main"
+        height = 0
+        for log in logs:
+            # "./" is a stationary operation
+            # => keep `height` the same
+            if log != "./":
+                # "../" is a backward operation
+                # => reduce `height` by 1
+                # but if we're already at Main, do nothing
+                if log == "../":
+                    height = max(height - 1, 0)
+                # otherwise, it's a normal forward operation
+                # => increase `height` by 1
+                else:
+                    height += 1
+
+        return height
